f73d515bc4f792624395193041d8a13540bf3df5
meo.con.batu <thanhmi1111@gmail.com>
2022-12-28 23:16:28 +0800

    refactor day19

--- 2022/day19/day19.go
+++ 2022/day19/day19.go
@@ -2,6 +2,7 @@ package day19
 
 import (
 	"fmt"
+	"math"
 	"time"
 
 	"github.com/meoconbatu/adventofcode/utils"
@@ -16,10 +17,10 @@ var dp map[int]int
 func (d Day19) Part1() {
 	blueprints := readInput()
 	rs := 0
-
 	s := time.Now()
 	for _, blueprint := range blueprints {
-		rs += blueprint[0] * maxGeodes(blueprint, 24)
+		temp := maxGeodes(blueprint, 24)
+		rs += blueprint[0] * temp
 	}
 	fmt.Println(rs, time.Since(s))
 }
@@ -28,7 +29,6 @@ func (d Day19) Part1() {
 func (d Day19) Part2() {
 	blueprints := readInput()
 	rs := 1
-
 	s := time.Now()
 	for i, blueprint := range blueprints {
 		if i >= 3 {
@@ -52,59 +52,129 @@ func readInput() [][7]int {
 }
 
 func maxGeodes(blueprint [7]int, mins int) int {
-	s := time.Now()
 	// ore, clay, obsidian, geode
-	fmt.Println(blueprint)
-
 	robots, gems := [4]int{1}, [4]int{}
 	dp = make(map[int]int)
-	rs := dfs(mins, gems, robots, blueprint)
+	g := 0
+	// dfs(mins, gems, robots, blueprint, &g)
+	return dfs(mins, gems, robots, blueprint, &g)
+}
+func bfs(blueprint [7]int) int {
+	q := []int{1, 0}
+	mins := 24
+	lenq := 2
+	rs := 0
+	visited := make(map[[3]int]bool)
+	for mins >= 0 {
+		robots, gems := q[0], q[1]
+		q = q[2:]
+		lenq -= 2
+		if mins == 0 {
+			rs = utils.Max(rs, get(gems, 3))
+		}
+		if get(gems, 0) >= blueprint[5] && get(gems, 2) >= blueprint[6] {
+			newgems := gems + robots - int(math.Pow(1000, 0))*blueprint[5] - int(math.Pow(1000, 2))*blueprint[6]
+			newrobots := robots + int(math.Pow(1000, 3)*1)
+			if !visited[[3]int{newrobots, newgems, mins}] {
+				q = append(q, newrobots, newgems)
+				visited[[3]int{newrobots, newgems, mins}] = true
+			}
 
-	fmt.Println(blueprint[0], rs)
-	fmt.Println(time.Since(s))
+		} else {
+			if get(gems, 0) >= blueprint[3] && get(gems, 1) >= blueprint[4] && (get(robots, 2) < blueprint[6] || get(robots, 2)*mins+get(gems, 2) < mins*blueprint[6]) {
+				newgems := gems + robots - int(math.Pow(1000, 0))*blueprint[3] - int(math.Pow(1000, 1))*blueprint[4]
+				newrobots := robots + int(math.Pow(1000, 2)*1)
+				if !visited[[3]int{newrobots, newgems, mins}] {
+					q = append(q, newrobots, newgems)
+					visited[[3]int{newrobots, newgems, mins}] = true
+				}
+			}
+			if get(gems, 0) >= blueprint[2] && (get(robots, 1) < blueprint[4] || get(robots, 1)*mins+get(gems, 1) < mins*blueprint[4]) {
+				newgems := gems + robots - int(math.Pow(1000, 0))*blueprint[2]
+				newrobots := robots + int(math.Pow(1000, 1)*1)
+				if !visited[[3]int{newrobots, newgems, mins}] {
+					q = append(q, newrobots, newgems)
+					visited[[3]int{newrobots, newgems, mins}] = true
+				}
+			}
+			if get(gems, 0) >= blueprint[1] && (get(robots, 0) < blueprint[3]+blueprint[5] || get(robots, 0)*mins+get(gems, 0) < mins*blueprint[3]) {
+				newgems := gems + robots - int(math.Pow(1000, 0))*blueprint[1]
+				newrobots := robots + 1
+				if !visited[[3]int{newrobots, newgems, mins}] {
+					q = append(q, newrobots, newgems)
+					visited[[3]int{newrobots, newgems, mins}] = true
+				}
+			}
+			if get(robots, 0)*mins+get(gems, 0) < mins*(blueprint[2]+blueprint[3]+blueprint[5]) {
+				newgems := gems + robots
+				newrobots := robots
+				if !visited[[3]int{newrobots, newgems, mins}] {
+					q = append(q, newrobots, newgems)
+					visited[[3]int{newrobots, newgems, mins}] = true
+				}
+			}
+		}
+		if lenq == 0 {
+			mins--
+			lenq = len(q)
+		}
+	}
 	return rs
 }
-
-func dfs(mins int, gems, robots [4]int, blueprint [7]int) int {
+func get(num, index int) int {
+	for i := 0; i < index; i++ {
+		num /= 1000
+	}
+	return num % 1000
+}
+func dfs(mins int, gems, robots [4]int, blueprint [7]int, maxGeode *int) int {
 	if mins == 0 {
+		*maxGeode = utils.Max(*maxGeode, gems[3])
 		return gems[3]
 	}
 	// TODO:
 	// don't know why use string as a key (the line below), some blueprints are wrong
 	// key := fmt.Sprintf("%v%v", gems, robots)
-	key := (mins << 4)
-	for i, robot := range robots {
-		key = (key << 4) | robot
+
+	key := mins
+	for i := 0; i < 3; i++ {
+		key = (key << 4) | robots[i]
 		key = (key << 4) | gems[i]
 	}
 	if v, ok := dp[key]; ok {
 		return v
 	}
+	if gems[3]+robots[3]+robots[3]*mins+(mins*(mins-1))/2 <= *maxGeode {
+		return 0
+	}
 	rs := 0
 	if gems[0] >= blueprint[5] && gems[2] >= blueprint[6] {
 		newgems := [4]int{gems[0] - blueprint[5] + robots[0], gems[1] + robots[1], gems[2] - blueprint[6] + robots[2], gems[3] + robots[3]}
 		newrobots := [4]int{robots[0], robots[1], robots[2], robots[3] + 1}
-		rs = utils.Max(rs, dfs(mins-1, newgems, newrobots, blueprint))
+		rs = utils.Max(rs, dfs(mins-1, newgems, newrobots, blueprint, maxGeode))
+	} else {
+		if gems[0] >= blueprint[3] && gems[1] >= blueprint[4] && (robots[2] < blueprint[6] || robots[2]*mins+gems[2] < mins*blueprint[6]) {
+			newgems := [4]int{gems[0] - blueprint[3] + robots[0], gems[1] - blueprint[4] + robots[1], gems[2] + robots[2], gems[3] + robots[3]}
+			newrobots := [4]int{robots[0], robots[1], robots[2] + 1, robots[3]}
+			rs = utils.Max(rs, dfs(mins-1, newgems, newrobots, blueprint, maxGeode))
+		}
+		if gems[0] >= blueprint[2] && (robots[1] < blueprint[4] || robots[1]*mins+gems[1] < mins*blueprint[4]) {
+			newgems := [4]int{gems[0] - blueprint[2] + robots[0], gems[1] + robots[1], gems[2] + robots[2], gems[3] + robots[3]}
+			newrobots := [4]int{robots[0], robots[1] + 1, robots[2], robots[3]}
+			rs = utils.Max(rs, dfs(mins-1, newgems, newrobots, blueprint, maxGeode))
+		}
+		if gems[0] >= blueprint[1] && (robots[0] < blueprint[3]+blueprint[5] || robots[0]*mins+gems[0] < mins*blueprint[3]) {
+			newgems := [4]int{gems[0] - blueprint[1] + robots[0], gems[1] + robots[1], gems[2] + robots[2], gems[3] + robots[3]}
+			newrobots := [4]int{robots[0] + 1, robots[1], robots[2], robots[3]}
+			rs = utils.Max(rs, dfs(mins-1, newgems, newrobots, blueprint, maxGeode))
+		}
+		if robots[0]*mins+gems[0] < mins*(blueprint[2]+blueprint[3]+blueprint[5]) {
+			for i, robot := range robots {
+				gems[i] += robot
+			}
+			rs = utils.Max(rs, dfs(mins-1, gems, robots, blueprint, maxGeode))
+		}
 	}
-	if gems[0] >= blueprint[3] && gems[1] >= blueprint[4] {
-		newgems := [4]int{gems[0] - blueprint[3] + robots[0], gems[1] - blueprint[4] + robots[1], gems[2] + robots[2], gems[3] + robots[3]}
-		newrobots := [4]int{robots[0], robots[1], robots[2] + 1, robots[3]}
-		rs = utils.Max(rs, dfs(mins-1, newgems, newrobots, blueprint))
-	}
-	if gems[0] >= blueprint[2] {
-		newgems := [4]int{gems[0] - blueprint[2] + robots[0], gems[1] + robots[1], gems[2] + robots[2], gems[3] + robots[3]}
-		newrobots := [4]int{robots[0], robots[1] + 1, robots[2], robots[3]}
-		rs = utils.Max(rs, dfs(mins-1, newgems, newrobots, blueprint))
-	}
-	if gems[0] >= blueprint[1] {
-		newgems := [4]int{gems[0] - blueprint[1] + robots[0], gems[1] + robots[1], gems[2] + robots[2], gems[3] + robots[3]}
-		newrobots := [4]int{robots[0] + 1, robots[1], robots[2], robots[3]}
-		rs = utils.Max(rs, dfs(mins-1, newgems, newrobots, blueprint))
-	}
-	for i, robot := range robots {
-		gems[i] += robot
-	}
-	rs = utils.Max(rs, dfs(mins-1, gems, robots, blueprint))
 	dp[key] = rs
 	return rs
 }
