ae0eedb2eb03ba6b1bf4be1fb9c9d8acd4c8ddc8
meo.con.batu <thanhmi1111@gmail.com>
2022-12-31 01:47:46 +0800

    optimize day23 (part2: down from 2s to ~800ms)

--- 2022/day23/day23.go
+++ 2022/day23/day23.go
@@ -2,6 +2,7 @@ package day23
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/meoconbatu/adventofcode/utils"
 )
@@ -11,21 +12,26 @@ type Day23 struct{}
 
 // Part1 func
 func (d Day23) Part1() {
-	grid, maxrow, maxcol := readInput()
-	minp, maxp := utils.Point{0, 0}, utils.Point{maxrow, maxcol}
+	grid := readInput()
 
 	idirection := 0
 	for round := 0; round < 10; round++ {
-		grid, _ = move(grid, &minp, &maxp, idirection)
+		grid, _ = move(grid, idirection)
 		idirection = (idirection + 1) % 4
 	}
 	rs := 0
-	for _, v := range grid {
+	minp, maxp := utils.Point{X: math.MaxInt64, Y: math.MaxInt64}, utils.Point{X: math.MinInt64, Y: math.MinInt64}
+	for pnum, v := range grid {
 		if v == 1 {
+			px, py := mapIntToXY(pnum)
+			minp.X = utils.Min(minp.X, px)
+			minp.Y = utils.Min(minp.Y, py)
+			maxp.X = utils.Max(maxp.X, px)
+			maxp.Y = utils.Max(maxp.Y, py)
 			rs++
 		}
 	}
-	fmt.Println((maxp.X-minp.X)*(maxp.Y-minp.Y) - rs)
+	fmt.Println((1+maxp.X-minp.X)*(1+maxp.Y-minp.Y) - rs)
 }
 
 func print(grid map[utils.Point]int, minp, maxp utils.Point) {
@@ -43,130 +49,91 @@ func print(grid map[utils.Point]int, minp, maxp utils.Point) {
 
 // Part2 func
 func (d Day23) Part2() {
-	grid, maxrow, maxcol := readInput()
-	minp, maxp := utils.Point{X: 0, Y: 0}, utils.Point{X: maxrow, Y: maxcol}
+	grid := readInput()
 
 	round := 0
 	for idirection, numMove := 0, 1; numMove != 0; idirection = (idirection + 1) % 4 {
-		grid, numMove = move(grid, &minp, &maxp, idirection)
+		grid, numMove = move(grid, idirection)
 		round++
 	}
 	fmt.Println(round)
 }
-func move(grid map[utils.Point]int, minp, maxp *utils.Point, idirection int) (map[utils.Point]int, int) {
-	grid2 := make(map[utils.Point]int, maxp.X)
-	from := make(map[utils.Point]utils.Point)
-	// direction := "NSWE"
-	xToNum, yToNum := make(map[int]int), make(map[int]int)
-	for i := minp.X; i < maxp.X; i++ {
-		for j := minp.Y; j < maxp.Y; j++ {
-			p := utils.Point{i, j}
-			if v, ok := grid[p]; !ok || (ok && v != 1) {
+
+func mapXYToInt(x, y int) int {
+	min := -200
+	m := 400
+	return (x-min)*m + y - min
+}
+func mapIntToXY(num int) (int, int) {
+	min := -200
+	m := 400
+	return num/m + min, num%m + min
+}
+
+var directions = [4][2]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+
+func move(grid map[int]byte, idirection int) (map[int]byte, int) {
+	grid2 := make(map[int]byte)
+	from := make(map[int]int)
+	for pnum, v := range grid {
+		if v != 1 {
+			continue
+		}
+		px, py := mapIntToXY(pnum)
+		totalMask, masks := getMask(grid, px, py)
+		canMove := false
+		for ii, icur := 0, idirection; totalMask != 0 && ii < 4; ii, icur = ii+1, (icur+1)%4 {
+			if masks[icur] != 0 {
 				continue
 			}
-			n := []utils.Point{{i - 1, j - 1}, {i - 1, j}, {i - 1, j + 1}}
-			s := []utils.Point{{i + 1, j - 1}, {i + 1, j}, {i + 1, j + 1}}
-			w := []utils.Point{{i - 1, j - 1}, {i, j - 1}, {i + 1, j - 1}}
-			e := []utils.Point{{i - 1, j + 1}, {i, j + 1}, {i + 1, j + 1}}
-			nswe := [][]utils.Point{n, s, w, e}
-			masks := [4]int{}
-			for ii, ps := range nswe {
-				mask := 0
-				for jj, p := range ps {
-					if grid[p] == 1 {
-						mask |= 1 << jj
-					}
-				}
-				masks[ii] = mask
+			ppx, ppy := px+directions[icur][0], py+directions[icur][1]
+			ppnum := mapXYToInt(ppx, ppy)
+			if v, ok := grid2[ppnum]; !ok {
+				canMove = true
+				grid2[ppnum] = 1
+				from[ppnum] = pnum
+			} else if v == 1 {
+				grid2[ppnum] = 2
+				grid2[from[ppnum]] = 1
+				delete(from, ppnum)
 			}
-			if masks[0]|masks[1]|masks[2]|masks[3] == 0 {
-				grid2[p] = 1
-				xToNum[p.X]++
-				yToNum[p.Y]++
-				continue
-			}
-			canMove := false
-			for ii, icur := 0, idirection; ii < 4; ii, icur = ii+1, (icur+1)%4 {
-				if masks[icur] == 0 {
-					pp := p
-					if icur == 0 {
-						pp.X--
-					} else if icur == 1 {
-						pp.X++
-					} else if icur == 2 {
-						pp.Y--
-					} else {
-						pp.Y++
-					}
-					if v, ok := grid2[pp]; !ok {
-						canMove = true
-						grid2[pp] = 1
-						from[pp] = p
-						xToNum[pp.X]++
-						yToNum[pp.Y]++
-					} else if v == 1 {
-						grid2[pp] = -1
-						xToNum[pp.X]--
-						yToNum[pp.Y]--
-
-						grid2[from[pp]] = 1
-						xToNum[from[pp].X]++
-						yToNum[from[pp].Y]++
-
-						delete(from, pp)
-					}
-					break
-				}
-			}
-			if !canMove {
-				grid2[p] = 1
-				xToNum[p.X]++
-				yToNum[p.Y]++
-			}
-		}
-	}
-	for i := -1; i <= 1; i++ {
-		if xToNum[minp.X+i] > 0 {
-			minp.X += i
 			break
 		}
-	}
-	for i := 1; i >= -1; i-- {
-		if xToNum[maxp.X-1+i] > 0 {
-			maxp.X += i
-			break
-		}
-	}
-	for i := -1; i <= 1; i++ {
-		if yToNum[minp.Y+i] > 0 {
-			minp.Y += i
-			break
-		}
-	}
-	for i := 1; i >= -1; i-- {
-		if yToNum[maxp.Y-1+i] > 0 {
-			maxp.Y += i
-			break
+		if !canMove {
+			grid2[pnum] = 1
 		}
 	}
 	return grid2, len(from)
 }
-func readInput() (map[utils.Point]int, int, int) {
+func getMask(grid map[int]byte, x, y int) (int, [4]int) {
+	n := []int{mapXYToInt(x-1, y-1), mapXYToInt(x-1, y), mapXYToInt(x-1, y+1)}
+	s := []int{mapXYToInt(x+1, y-1), mapXYToInt(x+1, y), mapXYToInt(x+1, y+1)}
+	w := []int{mapXYToInt(x-1, y-1), mapXYToInt(x, y-1), mapXYToInt(x+1, y-1)}
+	e := []int{mapXYToInt(x-1, y+1), mapXYToInt(x, y+1), mapXYToInt(x+1, y+1)}
+	nswe := [][]int{n, s, w, e}
+	masks := [4]int{}
+	totalMask := 0
+	for ii, ps := range nswe {
+		for jj, pp := range ps {
+			if grid[pp] == 1 {
+				masks[ii] |= 1 << jj
+			}
+		}
+		totalMask |= masks[ii]
+	}
+	return totalMask, masks
+}
+func readInput() map[int]byte {
 	scanner := utils.NewScanner(23)
-	grid := make(map[utils.Point]int, 0)
-	i := 0
-	maxrow := 0
-	maxcol := 0
-	for scanner.Scan() {
+	grid := make(map[int]byte, 0)
+	for i := 0; scanner.Scan(); i++ {
 		s := scanner.Text()
 		for j, r := range s {
 			if r == '#' {
-				grid[utils.Point{i, j}] = 1
-				maxrow = i
-				maxcol = utils.Max(maxcol, j)
+				num := mapXYToInt(i, j)
+				grid[num] = 1
 			}
 		}
-		i++
 	}
-	return grid, maxrow + 1, maxcol + 1
+	return grid
 }
